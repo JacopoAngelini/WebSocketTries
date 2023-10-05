ac8c39c6e033ccb036150b1642b71c38eaa0e334
Jacopo Angelini <jacopo.angelini@live.it>
2023-10-05 20:21:50 +0200

    installato express e prova con serve.listen

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const WebSocket = require("ws"); // include il modulo di ws: il principale server di websocket
+const http = require("http");
+const express =   require('express');
+const app = express();
+const server = http.createServer(app);
 
-wss = new WebSocket.Server({ port: 8082}); // crea il server specificando dove il server viene ospitato ovvero la porta 8082
+wss = new WebSocket.Server({ server:server });
+ // crea il server specificando dove il server viene ospitato ovvero la porta 8082
+
+server.listen(8082);
 
 // il server ascolta un evento, in questo caso basato sulla connesione di un utente al server tramite ''connection'', e performa l'azione nella callback
 
@@ -10,11 +17,13 @@ wss.on("connection", (ws) => {
   // quando arriva un messaggio da un utente (vedere index.js) performa un azione, i data vanno a finire in 'data'
   ws.on("message", (data) => {
     wss.clients.forEach(function each(client) {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(data.toString());
-        }  });
-  // perferma un azione quando si chiude la connessione, ovvero quando la pagina viene chiusa o ricaricata
-  ws.on("close", () => {
-    console.log("utente scollegato");
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data.toString());
+      }
+    });
+    // perferma un azione quando si chiude la connessione, ovvero quando la pagina viene chiusa o ricaricata
+    ws.on("close", () => {
+      console.log("utente scollegato");
+    });
   });
-})})
+});
